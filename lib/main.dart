bc0cfe72b8fa7970c0f09334b4bdccb998e18030
manak1 <m_ikeda@yumemi.co.jp>
2024-11-30 16:15:17 +0900

    feat: added information cards

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,76 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.deepPurple,
           body: SafeArea(
               child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('assets/images/me.png'),
               ),
-              Text(
+              const Text(
                 'manak1',
                 style: TextStyle(
                     fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Pacifico'),
+              ),
+              Text('WEB DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple.shade100,
+                      fontFamily: 'SourceSansPro',
+                      letterSpacing: 2.5)),
+              SizedBox(
+                width: 130,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.deepPurple.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.deepPurple.shade900,
+                      ),
+                      title: Text(
+                        'mana23happy@hotmail.co.jp',
+                        style: TextStyle(
+                          color: Colors.deepPurple.shade900,
+                          fontSize: 20.0,
+                        ),
+                      )),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ListTile(
+                        leading: Icon(
+                          Icons.link,
+                          color: Colors.deepPurple.shade900,
+                        ),
+                        title: Text(
+                          'https://github.com/manak1',
+                          style: TextStyle(
+                            color: Colors.deepPurple.shade900,
+                            fontSize: 20.0,
+                          ),
+                        ))),
               )
             ],
           ))),
